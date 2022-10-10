5f4b5595ccb539d4bbc0e8dde98b682e9531c1d8
Clayton <claytonadonis5@gmail.com>
2022-10-10 18:02:25 +0200

    Icon

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
+import 'vue-material-design-icons/styles.css';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,28 +2,162 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-grid>
+          <ion-row>
+            <ion-col class="title" size="11">
+              <ion-title>Dash Board</ion-title>
+            </ion-col>
+            <ion-col>
+              <ion-icon
+    name="search"
+    style="font-size: 24px; color: black"
+  />
+            </ion-col>
+          </ion-row>
+        </ion-grid>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+      <ion-grid class="content_grid">
+        <ion-row>
+          <ion-col size="6" v-for="item in items" :key="item.title">
+            <ion-router-link :to="item.to">
+              <ion-card>
+                <ion-card-content>
+                  {{item.title}}
+                </ion-card-content>
+              </ion-card>
+            </ion-router-link>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/vue';
+import { addIcons } from 'ionicons';
+import { searchCircle } from "ionicons/icons";
+// import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default defineComponent({
   name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonIcon },
+
+  data: () => ({
+    items: [
+      {
+        title: 'CRM',
+        img: "",
+        category: [
+          "admin"
+        ],
+        to: '/crm'
+      },
+      {
+        title: 'SRM',
+        img: "",
+        category: [
+          "admin"
+        ],
+        to: '/srm'
+      },
+      {
+        title: 'WO',
+        img: "",
+        category: [
+          "admin"
+        ],
+        to: '/wo'
+      },
+      {
+        title: 'Quotes',
+        img: "",
+        category: [
+          "admin"
+        ],
+        to: '/quotes'
+      },
+      {
+        title: 'PO',
+        img: "",
+        category: [
+          "admin"
+        ],
+        to: '/po'
+      },
+      {
+        title: 'Reports',
+        img: "",
+        category: [
+          "admin"
+        ],
+        to: '/reports'
+      },
+      {
+        title: 'Materials',
+        img: "",
+        category: [
+          "admin"
+        ],
+        to: '/materials'
+      },
+      {
+        title: 'Inventory',
+        img: "",
+        category: [
+          "admin"
+        ],
+        to: '/inventory'
+      },
+      {
+        title: 'Costs',
+        img: "",
+        category: [
+          "admin"
+        ],
+        to: '/costs'
+      }
+    ],
+  }),
+  created() {
+    addIcons({
+      'search': searchCircle,
+    });
+  }
 });
 </script>
+<style scoped>
+/* ion-card{
+
+ } */
+ion-content {
+  height: 100vh;
+}
+
+.content_grid {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+}
+
+ion-search-bar {
+  position: absolute;
+}
+
+ion-toolbar {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+ion-title {
+  font-size: 15px;
+}
+
+.title {
+  align-self: center;
+}
+</style>
